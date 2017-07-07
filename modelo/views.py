996aa3c57a44fee0029e0c1f88f456e1c5c3b482
Paulo <pauloitalo@hotmail.com.br>
2017-07-06 23:16:11 -0300

    Alteração do path

--- modelo/views.py
+++ modelo/views.py
@@ -7,7 +7,7 @@ from PIL import Image
 # Create your views here.
 
 def salvarFoto(foto):
-    path = 'home/projetoreconhecimentofacial/reconhecimentofacial/modelo/static/uploads/upload.jpg'
+    path = 'static/uploads/upload.jpg'
     with open(path, 'wb+') as wfoto:
         for chunk in foto.chunks():
             wfoto.write(chunk)
